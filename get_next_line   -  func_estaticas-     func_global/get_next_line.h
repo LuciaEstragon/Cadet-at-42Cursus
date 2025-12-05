81381328a02754667772f8bc6c564830fb747f44
Lucia Estrada Gonzalez <lestrada@c2r9s5.42madrid.com>
2025-12-05 20:09:53 +0100

    run

---
+++ get_next_line   -  func_estaticas-     func_global/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lestrada <lestrada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*                                                     #+#    #+#             */
+/*   Created: 2025/12/01 19:48:27 by lestrada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+int		handle_file_content(char *file, char *dictionary);
+int		open_and_read_file(char *file, char *buffer, int b_size);
+
+#endif
---
+++ printf  -                     func_variádicas/printf_05/ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lestrada <lestrada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 13:45:33 by lestrada          #+#    #+#             */
+/*   Updated: 2025/12/05 20:07:54 by lestrada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+/*
+var_type va_arg(va_list arg_ptr, var_type);
+void va_copy(va_list dest, va_list src);
+void va_end(va_list arg_ptr);
+void va_start(va_list arg_ptr, variable_name);
+void va_start(arg_ptr); 
+*/
+
+int			ft_printf(char const *s, ...);
+static int	function_types(char const *s, va_list arg);
+
+#endif
