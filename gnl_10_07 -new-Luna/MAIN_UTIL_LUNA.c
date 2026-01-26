9642ea298da5f74df7bd661cc597bcb07e4f2d4b
Lucia Estrada Gonzalez <lestrada@c1r8s1.42madrid.com>
2026-01-26 15:29:18 +0100

    df

---
+++ gnl_10_07 -new-Luna/MAIN_UTIL_LUNA.c
@@ -0,0 +1,53 @@
+///PARA ESCRIBIR DESDE fd = 0  
+//  - fd=0 es el terminal, estoy escribiendo desde teclado
+//  - scanf() 
+
+int main(void){
+	while (1) {
+		char *str = get_next_line(0);
+		if (str == NULL)
+			break;
+		printf("Acabas de escribir: %s\n", str);
+	}
+	return 0;
+}
+
+///PARA  ESCRIBIR UNA LINEA DEL fd
+//  - programa facilito
+
+int main(void){
+        int fd = open("file", O_RDONLY);
+        while (1) {
+                char *str = get_next_line(fd);
+                if (str == NULL)
+                        break;
+                printf("Acabas de escribir: %s\n", str);
+        }
+	close(fd);
+        return 0;
+}
+
+
+
+/* NOTAS SOBRE LOS LEAKS */
+1. En el Make file tienes que compilar con el flag -g3
+    Esto guarda informacion en el binarioo de salida
+   
+2. valgrind ./gnl
+    Esto me dice si hay errores de leaks
+ 
+3. valgrind --leak-check=full ./gnl 
+    Esto me dice cuantos errores
+
+4. valgrind --leak-check=full --show-leak-kinds=all ./gnl
+    Esto me dice donde estan esos errores -- lineas
+
+
+
+
+-> ERRORES DETECTDOS
+1. Si fd =0 - tiene que escribir info por pantalla
+2. si fd = NULL - si fd no existe o no tiene nada - return (0); - esto desde el principio, sin hacer llamada al read_line() - quue tiene un malloc
+3. si fd = a (sin \nl) 
+
+if (file < 0 || BUFFER_SIZE <= 0 || read(file, 0, 0) < 0)
---
+++ gnl_10_07 -new-Luna/get_next_line.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lestrada <lestrada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: Invalid date        by                   #+#    #+#             */
+/*   Updated: 2026/01/26 15:24:58 by lestrada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*
+GET_NEXT_LINE
+1) leo un determinado numero de caracteres del fichero (BUFFER_SIZE)
+	read_line(file, save);
+	1- leo de fichero BUFFER_SIZE caracteres
+	2- lo almaceno en str.
+	3- lo sumo a save (si save no tenia nada es de tamaño 1)
+
+2) crear una linea
+	make_line(save);
+	1- veo el contenido de todo lo que he guardado hasta que encuentro /n
+	  si lo encuentro: hasta aqui es mi nueva linea, lo demas lo guardo
+	  si no lo encuentro: se me va a devolver save entero
+	2- todo lo que no este guardado en line va a ser para guardar
+	  cojo desde la direccion de save(ind_maximo_linea) y la vuelvo 
+	  a guardar en save.
+	  Ejemplo: hola que tal?\n yo bien graci-
+	  line= hola que tal?
+	  save= yo bien graci-
+		* si no hubiera encontrado el salto de linea = strlen(linea)==
+		strlen(save) => no copiar nada en save ya es todo
+
+*/
+
+char	*get_next_line(int file)
+{
+	char		*line;
+	static char	*save = NULL;
+
+	if (file < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	save = read_file(file, save);
+	if (!save)
+		return (NULL);
+	if (!*save)
+	{
+		free(save);
+		save = NULL;
+		return (NULL);
+	}
+	line = make_line(&save);
+	return (line);
+}
+
+char	*ft_free_strjoin(char *save, char *tmp)
+{
+	char	*new;
+
+	if (!save || !tmp)
+		return (NULL);
+	new = ft_strjoin(save, tmp);
+	free(save);
+	return (new);
+}
+
+char	*read_file(int file, char *save)
+{
+	char	*str;
+	int		size_str;
+
+	if (!save)
+		save = (char *)ft_calloc(sizeof(char), 1);
+	str = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!str || !save)
+		return (free(str), free(save), NULL);
+	while (!ft_strchr(save, '\n'))
+	{
+		size_str = read(file, str, BUFFER_SIZE);
+		if (size_str < 0)
+			return (free(str), free(save), NULL);
+		else if (size_str == 0)
+			break ;
+		str[size_str] = '\0';
+		save = ft_free_strjoin(save, str); //no esta solucionando mis leaks
+		if (!save)
+			return (free(str), NULL);
+	}
+	free(str);
+	return (save);
+}
+//str[size_str] = '\0'; necesario para el tamaño del buffer ajustado, para limpiar el final del buffer cuando haga el join 
+
+char	*make_line(char **save)
+{
+	char	*line;
+	char	*save_aux;
+	char	*temp;
+	int		len;
+
+	if (!*save || (*save)[0] == '\0')
+		return (NULL);
+	save_aux = ft_strchr(*save, '\n');
+	if (save_aux == NULL)
+	{
+		line = ft_strdup(*save);
+		free(*save);
+		*save = NULL;
+		return (line);
+	}
+	len = save_aux - *save + 1;
+	line = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, *save, len + 1);
+	temp = ft_strdup(save_aux + 1);
+	free(*save);
+	*save = temp;
+	return (line);
+}
+// para qu te coja \n en la linea le pongo un +1 para que termine uno mas tarde
+// para que no te ccooja el \n en la linea le pongo que empiece desde +1
